e119baa0e43dcdd3ce0965a54abe0e3e66790fa3
user <muzidora>
2015-11-12 19:16:50 +0200

    first commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,16 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
+
+    /**
+     * @Route("/aa", name="aa")
+     *  Template()
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
 }
